133348f12a79476c6985cc2ce158a6798426f91a
nxgupta <neeraj.gupta6525@gmail.com>
2023-01-09 14:50:42 +0530

    Register/Login Page completed with toggle feature & Alert

--- client/src/components/Alert.jsx
+++ client/src/components/Alert.jsx
@@ -1,6 +1,10 @@
+import { useAppContext } from "../context/appContext"
 const Alert = () => {
+  const {
+     alertText,
+     alertType}=useAppContext();
   return (
-    <div className="alert alert-danger">alert goes here</div>
+    <div className={`alert alert-${alertType}`}>{alertText}</div>
   )
 }
 export default Alert
---
+++ client/src/context/appContext.jsx
@@ -0,0 +1,38 @@
+import {useState, useReducer, useContext, createContext} from 'react'
+import { CLEAR_ALERT, DISPLAY_ALERT } from './actions';
+import reducer from './reducer';
+
+const initialState={
+     isLoading:false,
+     showAlert: false,
+     alertText: '',
+     alertType: '',
+}
+
+const AppContext=createContext();
+
+const AppProvider=({children})=>{
+    const [state,dispatch]=useReducer(reducer,initialState)
+    
+    const displayAlert=()=>{
+        dispatch({type:DISPLAY_ALERT})
+        setTimeout(()=>{
+            clearAlert()
+        },3000)
+    }
+
+    const clearAlert=()=>{
+            dispatch({type:CLEAR_ALERT})
+    }
+
+    return <AppContext.Provider value={{...state,displayAlert,clearAlert}}>
+        {children}
+    </AppContext.Provider> 
+}
+
+//creating a use custom hook
+
+const useAppContext=()=>{
+    return useContext(AppContext)
+}
+export {AppProvider, initialState, useAppContext}
