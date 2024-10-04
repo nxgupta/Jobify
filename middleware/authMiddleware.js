5b7513d0c9954c607514a501bffbe7b05e09f3e5
Neeraj Kumar Gupta <gneeraj@mirabeltechnologies.com>
2024-10-04 11:51:58 +0530

    MES-profile-updation-completed

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,34 +1,30 @@
-import {
-  UnauthenticatedError,
-  UnauthorizedError,
-  BadRequestError,
-} from '../errors/customErrors.js';
-import { verifyJWT } from '../utils/tokenUtils.js';
+import {UnauthenticatedError, UnauthorizedError, BadRequestError} from "../errors/customErrors.js"
+import {verifyJWT} from "../utils/tokenUtils.js"
 
 export const authenticateUser = (req, res, next) => {
-  const { token } = req.cookies;
-  if (!token) throw new UnauthenticatedError('authentication invalid');
+  const {token} = req.cookies
+  if (!token) throw new UnauthenticatedError("authentication invalid")
 
   try {
-    const { userId, role } = verifyJWT(token);
-    const testUser = userId === '64b2c07ccac2efc972ab0eca';
-    req.user = { userId, role, testUser };
-    next();
+    const {userId, role} = verifyJWT(token)
+    const testUser = userId === "66ff824d5506ecf5057971b2"
+    req.user = {userId, role, testUser}
+    next()
   } catch (error) {
-    throw new UnauthenticatedError('authentication invalid');
+    throw new UnauthenticatedError("authentication invalid")
   }
-};
+}
 
 export const authorizePermissions = (...roles) => {
   return (req, res, next) => {
     if (!roles.includes(req.user.role)) {
-      throw new UnauthorizedError('Unauthorized to access this route');
+      throw new UnauthorizedError("Unauthorized to access this route")
     }
-    next();
-  };
-};
+    next()
+  }
+}
 
 export const checkForTestUser = (req, res, next) => {
-  if (req.user.testUser) throw new BadRequestError('Demo User. Read Only!');
-  next();
-};
+  if (req.user.testUser) throw new BadRequestError("Demo User. Read Only!")
+  next()
+}
--- middlewares/errorHandlerMiddleware.js
+++
@@ -1,9 +0,0 @@
-import {StatusCodes} from "http-status-codes"
-
-const errorHandleMiddleware = (err, req, res, next) => {
-  const statusCode = err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR
-  const msg = err.message || "something went wrong, please try again"
-  res.status(statusCode).json({msg})
-}
-
-export default errorHandleMiddleware
